4665cf494e99d054bcd374132f856cfb27d6b011
Lý Hoàng Long <101177931+longhoangcoder@users.noreply.github.com>
2022-10-19 09:46:53 +0700

    init first contract

---
+++ default_workspace/contracts/MyFirstApp.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.23;
+contract Hello {
+ 
+    string private message;
+    
+    function contructor(string mes) public {
+        message = mes;
+    }
+    
+    function setMessage(string mes) public {
+        message = mes;
+    }
+    
+    function getMessage() public view returns(string) {
+        return message;
+    }
+  
+}
